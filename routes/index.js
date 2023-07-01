595761d38b7d91530198fdf299d3130e343f25ef
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-07-01 14:41:02 +1000

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", function (req, res, next) {
+  const { message, author } = req.body;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect("/");
+});
+
+module.exports = router;
